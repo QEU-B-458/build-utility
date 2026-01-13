7f6af8b7aa62aad82b457708791539773429e26c
UnTamedStArBlOb <50789094+QEU-B-458@users.noreply.github.com>
2026-01-13 16:43:03 -0500

    Update wivrnkit.sh

--- scripts/wivrnkit.sh
+++ scripts/wivrnkit.sh
@@ -4,5 +4,5 @@
 ./distrobox-shims.sh
 
 # Update the container and install packages
-pacman -Syu
-grep -v '^#' ./boxkit.packages | xargs pacman -Syu 
+pacman -Syu --noconfirm
+grep -v '^#' ./boxkit.packages | xargs pacman -Syu --noconfirm
