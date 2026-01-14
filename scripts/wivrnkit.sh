a69ff70581520a541ee3d4bc71c8ea9c4134bd00
UnTamedStArBlOb <50789094+QEU-B-458@users.noreply.github.com>
2026-01-13 19:47:02 -0500

    Update wivrnkit.sh

--- scripts/wivrnkit.sh
+++ scripts/wivrnkit.sh
@@ -4,9 +4,7 @@
 ./distrobox-shims.sh
 
 # Update the container and install packages
-pacman -Syu --noconfirm --needed base-devel git wget
-git clone https://aur.archlinux.org/paru.git
-cd paru && makepkg -s --noconfirm && cd ..
+pacman -Syu --noconfirm --needed base-devel 
 grep -v '^#' ./wivrnkit.packages | xargs paru -S --noconfirm --needed
 
 wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O /usr/local/bin/appimagetool
